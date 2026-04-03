6666a3100ee0a2cb177ad446cf8ce8d5449079cd
Krushna Kale <119933455+KrushnaKale07@users.noreply.github.com>
2026-04-03 23:15:02 +0530

    Created a simple patient management program.
    8. Hospital Patient Record System
    Create a simple patient management program.
    Requirements
    ● Create a class Patient with:
    ○ patient ID
    ○ patient name
    ○ age
    ○ disease
    ● Create objects for at least 3 patients.
    ● Display patient details.
    ● Add a method to check whether the patient is a senior citizen (age 60 or above).

---
+++ Java_Assignment_Programs_Solutions/Java OOPAssignment2Solutions/HospitalPatientRecordSystem.java
@@ -0,0 +1,55 @@
+/*8. Hospital Patient Record System
+Create a simple patient management program.
+Requirements
+● Create a class Patient with:
+○ patient ID
+○ patient name
+○ age
+○ disease
+● Create objects for at least 3 patients.
+● Display patient details.
+● Add a method to check whether the patient is a senior citizen (age 60 or above).*/
+
+import java.util.Scanner;
+class HospitalPatientRecordSystem{
+	
+		int id;
+		String name;
+		int age;
+		String disease;
+		
+		HospitalPatientRecordSystem(int id, String name, int age, String disease ){
+			this.id = id;
+			this.name = name;
+			this.age = age;
+			this.disease = disease;
+		}
+		
+		public void seniorityCheck(){
+			if(age > 60){
+				System.out.println("Patient ID   : " + id);
+				System.out.println("Name         : " + name);
+				System.out.println("Age          : " + age);
+				System.out.println("Disease      : " + disease);
+				System.out.println("Patient is a senior citizen");
+				System.out.println();
+			}else{
+				System.out.println("Patient ID   : " + id);
+				System.out.println("Name         : " + name);
+				System.out.println("Age          : " + age);
+				System.out.println("Disease      : " + disease);
+				System.out.println("Patient is not a senior citizen");
+				System.out.println();
+			}
+		}
+		
+	public static void main(String [] args){		
+		HospitalPatientRecordSystem hprs1 = new HospitalPatientRecordSystem(1,"Raman Kumar", 65, "Appendix" );
+		hprs1.seniorityCheck();
+		HospitalPatientRecordSystem hprs2 = new HospitalPatientRecordSystem(2,"Karan Kumar", 50, "Appendix" );
+		hprs2.seniorityCheck();
+		HospitalPatientRecordSystem hprs3 = new HospitalPatientRecordSystem(3,"Varun Kumar", 25, "Appendix" );
+		hprs3.seniorityCheck();
+		
+	}
+}
