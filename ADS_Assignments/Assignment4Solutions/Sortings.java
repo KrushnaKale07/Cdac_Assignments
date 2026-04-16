d3042309e895dac211d89b0a7b69c6d3d33b0afd
Krushna Kale <119933455+KrushnaKale07@users.noreply.github.com>
2026-04-16 14:23:13 +0530

    Sorted array using bubble sort.
    Designed an application to sort an array using sorting techniques of data structure.

---
+++ ADS_Assignments/Assignment4Solutions/Sortings.java
@@ -0,0 +1,34 @@
+package com.pgcp.ads.assignment4;
+
+import java.util.Scanner;
+
+public class Sortings {
+
+	private static void bubbleSort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		System.out.println("After sorting using bubble sort.");
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the length of array.");
+		int length = scan.nextInt();
+		int arr[] = new int[length];
+		System.out.print("Please enter the element of array");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+		bubbleSort(arr);
+	}
+}
