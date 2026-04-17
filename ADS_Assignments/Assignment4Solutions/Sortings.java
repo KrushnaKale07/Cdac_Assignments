6b0720a2c358f7df06bccd15996e2e72e5ccfe3f
Krushna Kale <119933455+KrushnaKale07@users.noreply.github.com>
2026-04-18 00:06:31 +0530

    added logic to sort an array using insertion sort technique.

--- ADS_Assignments/Assignment4Solutions/Sortings.java
+++ ADS_Assignments/Assignment4Solutions/Sortings.java
@@ -20,6 +20,19 @@ public class Sortings {
 		}
 	}
 
+	private static void insertionSort(int[] arr) {
+
+		for (int i = 1; i < arr.length -1; i++) {
+			int j, temp = arr[i];
+			for (j = i - 1; j >= 0 && arr[j] > temp; j--) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j+1] = temp;
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter the length of array.");
@@ -29,6 +42,7 @@ public class Sortings {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = scan.nextInt();
 		}
-		bubbleSort(arr);
+		//bubbleSort(arr);
+		insertionSort(arr);
 	}
 }
