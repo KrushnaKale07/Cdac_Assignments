3669720d418668e6119870285ee700c3f0df375a
Krushna Kale <119933455+KrushnaKale07@users.noreply.github.com>
2026-04-18 01:38:56 +0530

    added logic to sort an array using merge sort technique.

--- ADS_Assignments/Assignment4Solutions/Sortings.java
+++ ADS_Assignments/Assignment4Solutions/Sortings.java
@@ -49,6 +49,50 @@ public class Sortings {
 		System.out.println(Arrays.toString(arr));
 	}
 
+	private static void mergeSort(int[] arr) {
+		devide(arr, 0, arr.length - 1);
+	}
+
+	private static void devide(int[] arr, int start, int end) {
+		if (start >= end) {
+			return;
+		}
+
+		int mid = start + (end - start) / 2;
+
+		devide(arr, start, mid);
+		devide(arr, mid + 1, end);
+		concure(arr, start, mid, end);
+	}
+
+	private static void concure(int[] arr, int start, int mid, int end) {
+		int merged[] = new int[end - start + 1];
+
+		int idx1 = start;
+		int idx2 = mid+1;
+		int x = 0;
+
+		while (idx1 <= mid && idx2 <= end) {
+			if (arr[idx1] <= arr[idx2]) {
+				merged[x++] = arr[idx1++];
+			} else {
+				merged[x++] = arr[idx2++];
+			}
+		}
+
+		while (idx1 <= mid) {
+			merged[x++] = arr[idx1++];
+		}
+
+		while (idx2 <= end) {
+			merged[x++] = arr[idx2++];
+		}
+		
+		for (int i = 0, j = start; i < merged.length; i++, j++) {
+			arr[j] = merged[i];
+		}
+	}
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Please enter the length of array.");
@@ -60,6 +104,9 @@ public class Sortings {
 		}
 		//bubbleSort(arr);
 		//insertionSort(arr);
-		selectionSort(arr);
+		//selectionSort(arr);
+
+		mergeSort(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 }
