c969300f9e1c163885e7ef31d642b74cc16dd350
Krushna Kale <119933455+KrushnaKale07@users.noreply.github.com>
2026-04-17 19:04:23 +0530

    added logic to print value/node in range.

--- ADS_Assignments/BST.java
+++ ADS_Assignments/BST.java
@@ -88,6 +88,20 @@ public class BST {
 		return root;
 	}
 
+	public static void printInRange(Node root, int x, int y) {
+		if (root == null) {
+			return;
+		}
+		if (root.data >= x && root.data <= y) {
+			printInRange(root.left, x, y);
+			System.out.print(root.data + " ");
+			printInRange(root.right, x, y);
+		} else if (root.data >= y) {
+			printInRange(root.left, x, y);
+		} else {
+			printInRange(root.right, x, y);
+		}
+	}
 
 	public static void main(String[] args) {
 		int values[] = { 8, 5, 1, 3, 4, 6, 10,11,14 };
@@ -100,8 +114,10 @@ public class BST {
 		inorder(root);
 		System.out.println();
 
-		delete(root, 14);
-		inorder(root);
+		printInRange(root, 6, 10);
+
+		//delete(root, 14);
+		//inorder(root);
 
 		/*if(search(root, 7)) {
 			System.out.println("Found");
