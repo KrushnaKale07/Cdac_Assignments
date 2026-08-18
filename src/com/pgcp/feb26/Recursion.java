4b76cb2b71ba749bfb6c8da5bb91264cc5f5ca09
Krushna <krushnakale0007@gmail.com>
2026-08-18 17:44:01 +0530

    added binary search logic

---
+++ src/com/pgcp/feb26/Recursion.java
@@ -0,0 +1,23 @@
+package com.pgcp.feb26;
+
+public class Recursion {
+
+	private static void demoRecursion() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void demoRecursion(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	demoRecursion.m1();
+		demoRecursion(5);
+		//demoRecursion
+
+	}
+
+}
