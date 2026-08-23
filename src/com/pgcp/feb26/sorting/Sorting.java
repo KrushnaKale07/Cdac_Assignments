10914d3860235570d1f70d90dc0ad90790dba5a6
Krushna <krushnakale0007@gmail.com>
2026-08-23 15:35:32 +0530

    added bubble sort logic

--- src/com/pgcp/feb26/sorting/Sorting.java
+++ src/com/pgcp/feb26/sorting/Sorting.java
@@ -4,8 +4,24 @@ public class Sorting {
 
 	public static void main(String[] args) {
 		int arr [] = { 5, 6, 3, 8, 1 };
-		selectionSort(arr);
-		System.out.println("Sorted array is: " + java.util.Arrays.toString(arr));
+//		selectionSort(arr);
+//		System.out.println("Selection Sorted array is: " + java.util.Arrays.toString(arr));
+		bubbleSort(arr);
+		System.out.println("Bubble Sorted array is: " + java.util.Arrays.toString(arr));
+	}
+
+	private static void bubbleSort(int[] arr) {
+		for(int j = 0; j < arr.length - 1; j++) {
+			for (int i = 0; i < arr.length-1; i++) {
+				if(arr[i] > arr[i + 1]) {
+					int temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+					
+				}
+			}
+			
+		}
 	}
 
 	private static void selectionSort(int[] arr) {
